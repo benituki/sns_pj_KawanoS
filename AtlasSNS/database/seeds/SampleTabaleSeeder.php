daeedaa1ca1a554c350fd7b5c36a67059d53f2ae
shiori123 <shioo5555@gmail.com>
2022-12-17 14:42:47 +0900

    seeder作成

---
+++ AtlasSNS/database/seeds/SampleTabaleSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SampleTabaleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
