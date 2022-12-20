c4a7ef0a431c43767b5bdc7ccc87a004eeed914d
shiori123 <shioo5555@gmail.com>
2022-12-21 04:18:57 +0900

    formの記述

--- AtlasSNS/config/app.php
+++ AtlasSNS/config/app.php
@@ -226,6 +226,11 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        //↓追加
+        //参照（https://course.lull-inc.co.jp/curriculum/8524/）
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+
     ],
 
 ];
--- AtlasSNS/resources/views/auth/added.blade.php
+++ AtlasSNS/resources/views/auth/added.blade.php
@@ -3,13 +3,19 @@
 @section('content')
 
 
+{!! Form::open() !!}
+
 <div id="clear">
-  <p>marumaruさん</p>
+  {{-- <p>marumaruさん</p>
   <p>ようこそ！AtlasSNSへ！</p>
   <p>ユーザー登録が完了しました。</p>
-  <p>早速ログインをしてみましょう。</p>
+  <p>早速ログインをしてみましょう。</p> --}}
+
+ 
 
   <p class="btn"><a href="/login">ログイン画面へ</a></p>
 </div>
+
+{!! Form::close() !!}
 
 @endsection
