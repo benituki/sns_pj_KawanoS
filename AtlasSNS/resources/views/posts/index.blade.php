023e0f1f07926bd71d46b45661926546e210af04
shiori123 <shioo5555@gmail.com>
2023-02-20 22:01:43 +0900

    フォームファサード編集

--- AtlasSNS/resources/views/posts/index.blade.php
+++ AtlasSNS/resources/views/posts/index.blade.php
@@ -3,19 +3,21 @@
 @section('content')
 <img src="{{ asset('/images/icon1.png') }}" >
 
-{{ Form::open() }}
+{!! Form::open() !!}
+{{ csrf_field() }}
 
 {{ Form::textarea('nate', null, ['class' => 'form-control', 'cols' => '150', 'rows' => '10', 'placeholder' => "投稿内容を入力してください。"])}}
+{{-- {{ Form::submit(('/public/images/post.png'), ['class' => 'btn'])}} --}}
 
-{{ Form::close() }}
+{!! Form::close() !!}
 
-<form action="/timeline" method="post">
+
+{{-- <form action="/timeline" method="post">
     {{ csrf_field() }}
         <div>
-            {{Form::text('inputName')}}
             <input type="text" name="tweet" style="margin: 0%; padding: 0 1rem; width: 70%; border-radius: 6px; border: 0px solid #ccc; height: 10rem;" placeholder="投稿内容を入力してください。">
             <button type="submit" style="background-color: #2695E0; color: white; border-radius: 10px; padding: 0.5rem;">ツイート</button>
         </div>
-    </form>
+    </form> --}}
 
 @endsection
