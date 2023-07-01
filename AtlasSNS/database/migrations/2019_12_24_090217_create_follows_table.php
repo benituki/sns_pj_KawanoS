1dd6509b81ef030ee65bcfbc6420928d2a28cc5a
shiori123 <shioo5555@gmail.com>
2023-07-01 17:45:21 +0900

    リレーションはモデル同士て行うため必要なし。

--- AtlasSNS/database/migrations/2019_12_24_090217_create_follows_table.php
+++ AtlasSNS/database/migrations/2019_12_24_090217_create_follows_table.php
@@ -3,14 +3,9 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use App\User;
 
 class CreateFollowsTable extends Migration
 {
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
     /**
      * Run the migrations.
      *
