724ffdf16f9845a77bc7df9950b9ca85ccc95236
shiori123 <shioo5555@gmail.com>
2023-07-01 16:16:41 +0900

    マイグレーション内の意味をメモした、

--- AtlasSNS/database/migrations/2019_12_24_090217_create_follows_table.php
+++ AtlasSNS/database/migrations/2019_12_24_090217_create_follows_table.php
@@ -14,10 +14,15 @@ class CreateFollowsTable extends Migration
     public function up()
     {
         Schema::create('follows', function (Blueprint $table) {
+            // idカラムを作成、autoIncrementはこのカラムが自動割分のプライマリキーであることを指す
             $table->integer('id')->autoIncrement();
+            // 'following_id'カラムを作成。integerメソッドは符号付整数カラムを定義する。フローを行うユーザーのIDを格納する。
             $table->integer('following_id');
+            // 'followed_id'というカラムを作成。フォローされる側のユーザーのIDを格納する。
             $table->integer('followed_id');
+            // 'created_at'というタイムスタンプカラムを作成。timestampメソッドはタイムスタンプを格納するためのカラムを定義する。useCurrentメソッドはこのカラムのdフォルト値として現在の日付を使用することを示す。
             $table->timestamp('created_at')->useCurrent();
+            // 'updated_at'というタイムスタンプカラムを作成。レコードが更新された時の日付を格納するために使用される。defaultメソッドを使用してカラムのｄフォルト値としてDB::rawメソッドを介して「current_timestamp on update current_timestamp」というデータベース固有の構文を指定しています。これにより、レコードが更新されたときに自動的に現在の日時が設定されます。
             $table->timestamp('updated_at')->default(DB::raw('current_timestamp on update current_timestamp'));
         });
     }
