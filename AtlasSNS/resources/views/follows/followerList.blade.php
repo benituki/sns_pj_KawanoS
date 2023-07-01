9e1cffa649cea6fb2b15b7be511939b09b373093
shiori123 <shioo5555@gmail.com>
2023-07-01 17:07:59 +0900

    IF文でフォロー機能を実施。（エラーが出てるため未実装

--- AtlasSNS/resources/views/follows/followerList.blade.php
+++ AtlasSNS/resources/views/follows/followerList.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
 <h1>フォロワーリスト</h1>
-@endsection
+@foreach ($followers as $follower)
+    <div>
+        <p>{{ $follower->name }}</p>
+        @if ($follower->is_following)
+            <form action="{{ route('unfollow', $follower->id) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit">フォロー解除</button>
+            </form>
+        @else
+            <form action="{{ route('follow', $follower->id) }}" method="POST">
+                @csrf
+                <button type="submit">フォローする</button>
+            </form>
+        @endif
+    </div>
+@endforeach
