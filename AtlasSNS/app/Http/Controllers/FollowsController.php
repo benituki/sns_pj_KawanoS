d83256d86dbe6035d2a97e636d7db473ecc3ef87
shiori123 <shioo5555@gmail.com>
2023-07-08 18:14:12 +0900

    不要なコードを削除、目も作成

--- AtlasSNS/app/Http/Controllers/FollowsController.php
+++ AtlasSNS/app/Http/Controllers/FollowsController.php
@@ -7,29 +7,14 @@ use App\User;
 
 class FollowsController extends Controller
 {
-    //
-    // public function follow(User $user)
-    // {
-    //     // フォローする処理
-    //     auth()->user()->following()->attach($user->id);
-
-    //     return back()->with('success', 'ユーザーをフォローしました。');
-    // }
-
-    // public function un_follow(User $user)
-    // {
-    //     // フォローを解除する処理
-    //     auth()->user()->following()->detach($user->id);
-
-    //     return back()->with('success', 'ユーザーのフォローを解除しました。');
-    // }
-
+    // フォロワーリスト用
     public function followerList(User $users)
     {
         $followers = $users->followers;
         return view('/follows/followerList', compact('followers'));
     }
 
+    // フォローリスト用
     public function followList(User $users)
     {
         $following = $users->following;
