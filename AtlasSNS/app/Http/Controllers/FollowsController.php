e8b9ab47a28c983339c899a11cd81e73a03dc0d0
shiori123 <shioo5555@gmail.com>
2023-09-03 17:25:19 +0900

    フォロー機能のコードについての役割をメモ、コードについて修正を加える。

--- AtlasSNS/app/Http/Controllers/FollowsController.php
+++ AtlasSNS/app/Http/Controllers/FollowsController.php
@@ -9,45 +9,57 @@ use Illuminate\Support\Facades\Auth;
 class FollowsController extends Controller
 {
     // フォロワーリスト用
-    public function followerList(User $users)
+    public function followerList(User $id)
     {
-        $followers = $users->followers;
+        $followers = $id->followers;
         return view('/follows/followerList', compact('followers'));
     }
 
     // フォローリスト用
-    public function followList(User $users)
+    public function followList(User $id)
     {
-        $following = $users->following;
+        $following = $id->following;
         return view('/follows/followList', compact('following'));
     }
 
     public function follow($id)
     {
         // ログイン中のユーザーを取得
-        $loggedInUser = Auth::users();
+        $loggedInUser = Auth::user();
+        // Auth::user()：Laravelの認証システムを介して現在ログインしているユーザーの情報を取得します。
+        // $loggedInUser：取得したログイン中のユーザー情報を格納するための変数です。
 
         // フォローするユーザーを取得
         $userToFollow = User::find($id);
+        // User::find($id)：User モデルを使用して、データベース内で指定されたユーザーID ($id) に対応するユーザーのレコードを検索・取得します。
+        // find は、データベースから特定のレコードを取得するために一般的に使用されるメソッドの一つ
+        // $userToFollow：取得したユーザー情報を格納するための変数です。この変数には、データベースから取得したユーザーの情報が含まれます。
 
         // フォロー関係を保存
         $loggedInUser->following()->attach($userToFollow);
+        // $loggedInUser：ログインしているユーザーを表すEloquentモデルのインスタンスです。通常、これはログインしているユーザーの情報を持っています。
+        // following()：Eloquentのリレーションシップメソッドで、$loggedInUser がフォローしているユーザー（フォロー中のユーザー）を表します。このメソッドは通常、User モデル内で定義されています。
+        // attach($userToFollow)：following() リレーションシップに対して、指定したユーザー（$userToFollow）を関連付けます。つまり、$loggedInUser が $userToFollow をフォローしたことをデータベースに保存します。
 
-        return redirect()->back(); // フォロー後に元のページに戻る
+        return redirect()->back();  // フォロー後に元のページに戻る
+        // redirect()：Laravelのリダイレクトを行うためのメソッドです。これを呼び出すことで、リダイレクト用のリダイレクトレスポンスが生成されます。
+        // back()：back() メソッドは、ユーザーを前のURLに戻すための便利な方法です。これにより、現在のリクエストのリファラ（リクエストが発生した前のURL）にリダイレクトされます。一般的に、ユーザーがフォームを送信した後や、あるアクションを実行した後に元のページに戻るのに使用されます。
     }
+
+    // 役割を直す→解除機能実装（レコードが削除されたら官僚）→ボタン機能をつける。
 
     public function un_follow($id)
     {
         // ログイン中のユーザーを取得
-        $loggedInUser = Auth::users();
+        $loggedInUser = Auth::user();
 
         // フォロー解除するユーザーを取得
-        $userToUn_follow = User::find($id);
+        $userToFollow = User::find($id);
 
         // フォロー関係を解除
-        $loggedInUser->following()->detach($userToUn_follow);
+        $loggedInUser->following()->detach($userToFollow);
 
-        return redirect()->back(); // フォロー解除後に元のページに戻る
+        return redirect()->back();  // フォロー解除後に元のページに戻る
     }
 
 
--- AtlasSNS/app/User.php
+++ AtlasSNS/app/User.php
@@ -46,12 +46,19 @@ class User extends Authenticatable
     // フォローボタンの切り替えのためである。フォローボタン機能自体はリレーション
     public function followers()
     {
-        return $this->belongsToMany(User::class, 'follows', 'following_id', 'followed_id');
+        return $this->belongsToMany(User::class, 'follows', 'followed_id', 'following_id');
     }
+    // followers() メソッド：このメソッドは「フォロワー」を表す関数です。つまり、このユーザーをフォローしている他のユーザーを取得します。
+    // return $this->belongsToMany(User::class, 'follows', 'following_id', 'followed_id');：これはEloquentの「多対多」のリレーションシップを設定しています。このメソッドは、User モデルが他の User モデルと多対多の関係を持つことを示しています。
+    // User::class：リレーションシップの相手先モデルを指定しています。この場合、他のユーザーモデルとの関係を示しています。
+    // 'follows'：中間テーブルの名前を指定しています。中間テーブルは、フォロー関係を格納するためのテーブルです。
+    // 'following_id' と 'followed_id'：中間テーブル内の外部キーを指定しています。
+    // following_id' はこのユーザー（自身）をフォローしている他のユーザーを関連付け、
+    // 'followed_id' はこのユーザーがフォローしている他のユーザーを関連付けます。
 
     public function following()
     {
-        return $this->belongsToMany(User::class, 'follows', 'followed_id', 'following_id');
+        return $this->belongsToMany(User::class, 'follows', 'following_id', 'followed_id');
     }
 
 }
