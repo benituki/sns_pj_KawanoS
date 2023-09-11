3be3af1a424f3478664412716a9f5eae9d29f09f
shiori123 <shioo5555@gmail.com>
2023-09-11 21:20:52 +0900

    自分自身を検索結果に出ないようにする

--- AtlasSNS/app/Http/Controllers/UsersController.php
+++ AtlasSNS/app/Http/Controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use APP\Post;
 use App\User;
 
@@ -14,36 +15,64 @@ class UsersController extends Controller
     }
 
     // 検索
-    public function search(Request $request){
-        // return view('users.search');
-        // return viewは、laravelにおけるビューの表示を行うためのコード
-        // このコードはusers/search.blade.phpというビューファイルを表示するために使用される。
-        // view()関数はlaravelでビューを表示するために使用されるヘルパー関数。この関数に引数としてビューファイルのパスを指定することで、該当するビューファイルを表示する準備が整う。
+    // public function search(Request $request){
+    //     // return view('users.search');
+    //     // return viewは、laravelにおけるビューの表示を行うためのコード
+    //     // このコードはusers/search.blade.phpというビューファイルを表示するために使用される。
+    //     // view()関数はlaravelでビューを表示するために使用されるヘルパー関数。この関数に引数としてビューファイルのパスを指定することで、該当するビューファイルを表示する準備が整う。
 
-        // ユーザー一覧をページネートで習得
-        $users = User::paginate(20);
-        // 検索フォームで入力された値を習得する
+    //     // ユーザー一覧をページネートで習得
+    //     $users = User::paginate(20);
+    //     // 検索フォームで入力された値を習得する
+    //     $search = $request->input('search');
+    //     // クエリビルダ
+    //     $query = User::query();
+    //     // もし検索フォームのキーワードが入力されたら
+    //     if($search){
+    //         // 全角スペースを半角に変換
+    //         $spaceConversion = mb_convert_kana($search, 's');
+    //         // 単語を半角スペースを区切り、配列にする（例：”川野 詩織 -> ["川野","詩織"])
+    //         $wordArraySearched = preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
+    //         // 単語をループで回し、ユーザーネームとぶっ分一意するものがあれば、$queryとして保持される
+    //         foreach($wordArraySearched as $value) {
+    //             $query->where('username', 'like', '%'.$value.'%');
+    //         }
+    //         // 上記で習得した$queryをページネートにし変数$usersに代入
+    //         $users = $query->paginate(20);
+    //     }
+    //     // ビューにusersとsearchを変数として渡す
+    //     return view('users.search', ['users' => $users])
+    //     ->with([
+    //         'users' => $users,
+    //         'search' => $search,
+    //     ]);
+    // }
+
+    public function search(Request $request) {
         $search = $request->input('search');
+        // ログインユーザーのIDを取得
+        $loggedInUserId = Auth::id();
         // クエリビルダ
         $query = User::query();
         // もし検索フォームのキーワードが入力されたら
-        if($search){
+        if ($search) {
             // 全角スペースを半角に変換
             $spaceConversion = mb_convert_kana($search, 's');
-            // 単語を半角スペースを区切り、配列にする（例：”川野 詩織 -> ["川野","詩織"])
+            // 単語を半角スペースを区切り、配列にする
             $wordArraySearched = preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
-            // 単語をループで回し、ユーザーネームとぶっ分一意するものがあれば、$queryとして保持される
-            foreach($wordArraySearched as $value) {
-                $query->where('username', 'like', '%'.$value.'%');
+            // 単語をループで回し、ユーザーネームと一致するものがあれば$queryに追加
+            foreach ($wordArraySearched as $value) {
+                $query->where('username', 'like', '%' . $value . '%');
             }
-            // 上記で習得した$queryをページネートにし変数$usersに代入
+            // 自分自身を除外する条件を追加
+            $query->where('id', '!=', $loggedInUserId);
+            // ページネート
             $users = $query->paginate(20);
+        } else {
+            // もし検索フォームにキーワードが入力されていない場合、自分以外のユーザーをすべて取得
+            $users = User::where('id', '!=', $loggedInUserId)->paginate(20);
         }
         // ビューにusersとsearchを変数として渡す
-        return view('users.search', ['users' => $users])
-        ->with([
-            'users' => $users,
-            'search' => $search,
-        ]);
+        return view('users.search', compact('users', 'search'));
     }
 }
