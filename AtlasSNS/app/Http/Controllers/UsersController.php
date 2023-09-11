a8d9c0461fb72e114a78f8a9af0a717f92672f48
shiori123 <shioo5555@gmail.com>
2023-09-11 21:36:13 +0900

    必要ではなくなったコードを削除。$search = $request->input('search');についてのメモを入力

--- AtlasSNS/app/Http/Controllers/UsersController.php
+++ AtlasSNS/app/Http/Controllers/UsersController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// 下記はuse文が不足していたため追加。
 use Illuminate\Support\Facades\Auth;
+//
 use APP\Post;
 use App\User;
 
@@ -14,43 +16,14 @@ class UsersController extends Controller
         return view('users.profile');
     }
 
-    // 検索
-    // public function search(Request $request){
-    //     // return view('users.search');
-    //     // return viewは、laravelにおけるビューの表示を行うためのコード
-    //     // このコードはusers/search.blade.phpというビューファイルを表示するために使用される。
-    //     // view()関数はlaravelでビューを表示するために使用されるヘルパー関数。この関数に引数としてビューファイルのパスを指定することで、該当するビューファイルを表示する準備が整う。
-
-    //     // ユーザー一覧をページネートで習得
-    //     $users = User::paginate(20);
-    //     // 検索フォームで入力された値を習得する
-    //     $search = $request->input('search');
-    //     // クエリビルダ
-    //     $query = User::query();
-    //     // もし検索フォームのキーワードが入力されたら
-    //     if($search){
-    //         // 全角スペースを半角に変換
-    //         $spaceConversion = mb_convert_kana($search, 's');
-    //         // 単語を半角スペースを区切り、配列にする（例：”川野 詩織 -> ["川野","詩織"])
-    //         $wordArraySearched = preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
-    //         // 単語をループで回し、ユーザーネームとぶっ分一意するものがあれば、$queryとして保持される
-    //         foreach($wordArraySearched as $value) {
-    //             $query->where('username', 'like', '%'.$value.'%');
-    //         }
-    //         // 上記で習得した$queryをページネートにし変数$usersに代入
-    //         $users = $query->paginate(20);
-    //     }
-    //     // ビューにusersとsearchを変数として渡す
-    //     return view('users.search', ['users' => $users])
-    //     ->with([
-    //         'users' => $users,
-    //         'search' => $search,
-    //     ]);
-    // }
-
     public function search(Request $request) {
         $search = $request->input('search');
         // ログインユーザーのIDを取得
+        // $requestオブジェクトはHTTPリクエストに関する情報を持つオブジェクト（例；ユーザーからのフォーム送信やGETリクエストなどが含まれる）
+        // HTTPリクエストとは：GET、POST、PUT（PATCH）、DELETEなどのweb上の城尾法を取得したりデータを送信したりするために使用される。
+        //input('search')は$requestオブジェクトから指定されたキー（ここではsearch）に関するデータを取得しようとする。
+        // 取得したデータは変数$searchに代入される。この変数にはユーザーが検索フォームに入力したキーワードやテキストが格納されている。
+        // 例；ユーザーが検索フォームに”川野詩織”と入力した場合$searchには’川野詩織’という文字列が格納される。これにより後続のコードで検索にしゆおう出来るようになる。
         $loggedInUserId = Auth::id();
         // クエリビルダ
         $query = User::query();
