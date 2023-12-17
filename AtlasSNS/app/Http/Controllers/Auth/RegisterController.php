8aec143a7a2ed8cd87c48299d679b441b08dda12
shiori123 <shioo5555@gmail.com>
2023-12-17 16:51:38 +0900

    初期ユーザーアイコンを設定。（念のための設定である）

--- AtlasSNS/app/Http/Controllers/Auth/RegisterController.php
+++ AtlasSNS/app/Http/Controllers/Auth/RegisterController.php
@@ -78,6 +78,7 @@ class RegisterController extends Controller
             'username' => $data['username'],
             'mail' => $data['mail'],
             'password' => bcrypt($data['password']),
+            'images' => 'images/CqT6aWcJZkNYUfG8EovYNT3rZXCP4eWSCtxvD6qy.png',
         ]);
 
     }
