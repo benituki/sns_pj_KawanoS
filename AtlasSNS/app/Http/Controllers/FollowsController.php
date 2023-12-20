62dadcbe0fc14db56e3cde8de0ab1c77a15f99df
shiori123 <shioo5555@gmail.com>
2023-12-20 16:36:00 +0900

    検索画面用のフォロー、フォロー解除の処理を追加

--- AtlasSNS/app/Http/Controllers/FollowsController.php
+++ AtlasSNS/app/Http/Controllers/FollowsController.php
@@ -63,7 +63,7 @@ class FollowsController extends Controller
         // following()：Eloquentのリレーションシップメソッドで、$loggedInUser がフォローしているユーザー（フォロー中のユーザー）を表します。このメソッドは通常、User モデル内で定義されています。
         // attach($userToFollow)：following() リレーションシップに対して、指定したユーザー（$userToFollow）を関連付けます。つまり、$loggedInUser が $userToFollow をフォローしたことをデータベースに保存します。
 
-        return redirect()->back();  // フォロー後に元のページに戻る
+        return redirect()->back()->withInput();  // フォロー後に元のページに戻る
         // redirect()：Laravelのリダイレクトを行うためのメソッドです。これを呼び出すことで、リダイレクト用のリダイレクトレスポンスが生成されます。
         // back()：back() メソッドは、ユーザーを前のURLに戻すための便利な方法です。これにより、現在のリクエストのリファラ（リクエストが発生した前のURL）にリダイレクトされます。一般的に、ユーザーがフォームを送信した後や、あるアクションを実行した後に元のページに戻るのに使用されます。
     }
@@ -79,7 +79,35 @@ class FollowsController extends Controller
         // フォロー関係を解除
         $loggedInUser->following()->detach($userToFollow);
 
-        return redirect()->back();  // フォロー解除後に元のページに戻る
+        return redirect()->back()->withInput();  // フォロー解除後に元のページに戻る
+    }
+
+    public function search_follow($id)
+    {
+        // ログイン中のユーザーを取得
+        $loggedInUser = Auth::user();
+
+        // フォローするユーザーを取得
+        $userToFollow = User::find($id);
+
+        // フォロー関係を保存
+        $loggedInUser->following()->attach($userToFollow);
+
+        return redirect('/search');
+    }
+
+    public function search_un_follow($id)
+    {
+        // ログイン中のユーザーを取得
+        $loggedInUser = Auth::user();
+
+        // フォロー解除するユーザーを取得
+        $userToFollow = User::find($id);
+
+        // フォロー関係を解除
+        $loggedInUser->following()->detach($userToFollow);
+
+        return redirect('/search');
     }
 
 
