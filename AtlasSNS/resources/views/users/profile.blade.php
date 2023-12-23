341f422e2f399e70b2d0ffe4c05322414c968c80
shiori123 <shioo5555@gmail.com>
2023-12-23 14:45:04 +0900

    プロフィール編集画面のコードをさらに細かくclass分けをする。

--- AtlasSNS/resources/views/users/profile.blade.php
+++ AtlasSNS/resources/views/users/profile.blade.php
@@ -1,101 +1,124 @@
 @extends('layouts.login')
 @section('content')
-<div class="card w-50 mx-auto m-5">
-    <div class="card-body">
-        <div class="pt-2">
-            <p class="h3 border-bottom border-secondary pb-3">
-                @if(Auth::check() && Auth::user()->id == $user->id)
-                    プロフィール編集
-                @else
 
-                @endif
-            </p>
-        </div>
-        <div class="profile-edit">
+<div class="card">
+    <div class="pt-2">
+        <p class="profile_if">
             @if(Auth::check() && Auth::user()->id == $user->id)
+
+            @else
+
+            @endif
+        </p>
+    </div>
+    <div class="profile">
+        @if(Auth::check() && Auth::user()->id == $user->id)
+        <div class="profile_edit_group">
             <img src="/storage/{{$user->images}}">
             {!! Form::open(['route' => ['profile_edit'], 'method' => 'PUT', "enctype" => "multipart/form-data"])!!}
             {!! Form::hidden('id', $user->id) !!}
-            <div class="m-3">
-                <div class="form-group pt-1">
-                    {{Form::label('username', 'username')}}
-                    {{Form::text('username', $user->username, ['class' => 'form-control', 'id' => 'username'])}}
+            <div class="profile_edit">
+                <div class="form-container">
+                    <div class="form-group">
+                        {{Form::label('username', 'user name')}}
+                        {{Form::text('username', $user->username, ['class' => 'form-control', 'id' => 'username'])}}
+                        <span class="text-danger">{{$errors->first('username')}}</span>
+                </div>
+                <div class="form-container">
+                    <div class="form-group">
+                        {{Form::label('username', 'user name')}}
+                        {{Form::text('username', $user->username, ['class' => 'form-control', 'id' => 'username'])}}
+                    </div>
                     <span class="text-danger">{{$errors->first('username')}}</span>
                 </div>
-                <div class="form-group pt-2">
-                    {{Form::label('mail', 'mail')}}
-                    {{Form::text('mail', $user->mail, ['class' => 'form-control', 'id' => 'mail'])}}
+                <div class="form-container">
+                    <div class="form-group">
+                        {{Form::label('mail', 'adress mail')}}
+                        {{Form::text('mail', $user->mail, ['class' => 'form-control', 'id' => 'mail'])}}
+                    </div>
                     <span class="text-danger">{{$errors->first('mail')}}</span>
                 </div>
-                <div class="form-group pt-2">
-                    {{Form::label('password', 'password')}}
-                    {{Form::password('password', ['class' => 'form-control', 'id' => 'password'])}}
+                <div class="form-container">
+                    <div class="form-group">
+                        {{Form::label('password', 'password')}}
+                        {{Form::password('password', ['class' => 'form-control', 'id' => 'password'])}}
+                    </div>
                     <span class="text-danger">{{$errors->first('password')}}</span>
                 </div>
-                <div class="form-group pt-1">
-                    {{Form::label('password-confirm', 'password confirm')}}
-                    {{Form::password('password-confirm', ['class' => 'form-control', 'id' => 'password-confirm'])}}
+                <div class="form-container">
+                    <div class="form-group">
+                        {{Form::label('password-confirm', 'password confirm')}}
+                        {{Form::password('password-confirm', ['class' => 'form-control', 'id' => 'password-confirm'])}}
+                    </div>
                     <span class="text-danger">{{$errors->first('password-confirm')}}</span>
                 </div>
-                <div class="form-group pt-3">
-                    {{Form::label('bio', 'bio')}}
-                    {{Form::text('bio', $user->bio, ['class' => 'form-control', 'id' => 'bio'])}}
+                <div class="form-container">
+                    <div class="form-group">
+                        {{Form::label('bio', 'bio')}}
+                        {{Form::text('bio', $user->bio, ['class' => 'form-control', 'id' => 'bio'])}}
+                    </div>
                     <span class="text-danger">{{$errors->first('bio')}}</span>
+                    </div>
                 </div>
-                <div class="form-group pt-3" >
-                    {{Form::label('images', 'icon image')}}
-                    {{Form::file('images', ['class' => 'form-control', 'id' => 'images'])}}
+                <div class="form-container">
+                    <div class="form-group_img" >
+                        {{Form::label('images', 'icon image')}}
+                        {{Form::file('images', ['class' => 'form-control', 'id' => 'images'])}}
+                    </div>
                     <span class="text-danger">{{$errors->first('images')}}</span>
                 </div>
-                <div class="form-group pull-right">
-                    {{Form::submit('更新する', ['class'=>'btn btn-success rounded-pill'])}}
+            </div>
+        <div class="form-btn">
+            {{Form::submit('更新', ['class'=>'form-btn'])}}
+        </div>
+        {!!Form::close()!!}
+        </div>
+
+        @else
+
+        <div class="profiles">
+            <div class="profile_detail">
+                <img src="/storage/{{$user->images}}">
+                <ul>
+                    <div class="profile_name">
+                        <li>
+                            <h1>name</h1>
+                            <p>{{ $user->username }}</p>
+                        </li>
+                    </div>
+                    <div class="profile_bio">
+                        <li>
+                            <h1>bio</h1>
+                            <p>{{ $user->bio }}</p>
+                        </li>
+                    </div>
+                </ul>
+                <div class="follow-btn">
+                    {{-- 下記追加（IF文） --}}
+                    @if(Auth::user()->isFollowing($user->id))
+                    {{-- フォロー解除ボタン --}}
+                    <form action="{{ route('un_follow', ['id' => $user->id]) }}" method="POST">
+                        {{ csrf_field() }}
+                        <button type="submit" class="unfollow-btn">フォロー解除</button>
+                    </form>
+                    @else
+                    {{-- フォローボタン --}}
+                    <form action="{{ route('follow', ['id' => $user->id]) }}" method="POST">
+                        {{ csrf_field() }}
+                        <button type="submit" class="newfollow-btn">フォローする</button>
+                    </form>
+                    @endif
+                    {{-- 上記追加（IF分） --}}
                 </div>
             </div>
-            {!!Form::close()!!}
-        </div>
 
-        @else
-
-        <div class="profile-detail">
-            <img src="/storage/{{$user->images}}">
-            <ul>
-                <li>
-                    <p>name</p>
-                </li>
-                <p>{{ $user->username }}</p>
-                <li>
-                    <p>bio</p>
-                </li>
-                <p> {{ $user->bio }}</p>
-            </ul>
-
-        </div>
-    <div class="follow-btn">
-         {{-- 下記追加（IF文） --}}
-        @if(Auth::user()->isFollowing($user->id))
-         {{-- フォロー解除ボタン --}}
-        <form action="{{ route('un_follow', ['id' => $user->id]) }}" method="POST">
-            {{ csrf_field() }}
-            <button type="submit">フォロー解除</button>
-        </form>
-        @else
-        {{-- フォローボタン --}}
-        <form action="{{ route('follow', ['id' => $user->id]) }}" method="POST">
-            {{ csrf_field() }}
-            <button type="submit">フォローする</button>
-        </form>
-        @endif
-        {{-- 上記追加（IF分） --}}
-    </div>
-
-    <div class="line"></div>
-
-    {{-- プロフィール詳細画面 --}}
-    <div class="container">
-        <div>
-            @if ($user->posts->count() > 0)
-                <ul>
-                    @foreach ($user->posts as $tweet)
+            <div class="line"></div>
+            {{-- プロフィール詳細画面 --}}
+            <div class="container">
+                <div>
+                    @if ($user->posts->count() > 0)
+                    <ul>
+                        @foreach ($user->posts as $tweet)
                         <li class="post-follow-block">
                             <figure>
                                 <a href="{{ route('profile', [$tweet->user->id]) }}">
@@ -110,22 +133,16 @@
                                 <div>{{ $tweet->post }}</div>
                             </div>
                         </li>
-                    @endforeach
-                </ul>
-                @else
-                <p>{{ $user->username }}さんはまだ何も呟いていません。</p>
-                @endif
-            </li>
-            @endif
+                        @endforeach
+                    </ul>
+                    @else
+                    <p>{{ $user->username }}さんはまだ何も呟いていません。</p>
+                    @endif
+                </div>
+            </div>
         </div>
+        @endif
     </div>
-
-
-    </div>
-
-
-
-
 </div>
 
 @if($errors->any())
