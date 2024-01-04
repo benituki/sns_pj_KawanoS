84288d9b98da34fd5e1fd39b8528ed78e1c42979
shiori123 <shioo5555@gmail.com>
2024-01-04 20:56:06 +0900

    コードを整理。

--- AtlasSNS/resources/views/auth/login.blade.php
+++ AtlasSNS/resources/views/auth/login.blade.php
@@ -23,36 +23,27 @@
 @extends('layouts.logout')
 
 @section('content')
-
-
-
-    <div class="form-group">
-
-        {!! Form::open() !!}
+<div class="form-group">
         
-        <p>AtlasSNSへようこそ</p>
-
-        <div class="form-item">
-            {{ Form::label('mail', 'mail address') }}
-            {{ Form::text('mail', null, ['class' => 'input']) }}
-        </div>
-        
-        <div class="form-item">
-            {{ Form::label('password', 'password') }}
-            {{ Form::password('password', ['class' => 'input']) }}
-        </div>
-
-        <div class="form-item">
-            {{ Form::submit('LOGIN', ['class' => 'submit-button']) }}
-        </div>
-
-        <p><a href="/register">新規ユーザーの方はこちら</a></p>
-
-        {!! Form::close() !!}
+    {!! Form::open() !!}
+    
+    <p>AtlasSNSへようこそ</p>
+    
+    <div class="form-item">
+        {{ Form::label('mail', 'mail address') }}
+        {{ Form::text('mail', null, ['class' => 'input']) }}
     </div>
 
+    <div class="form-item">
+        {{ Form::label('password', 'password') }}
+        {{ Form::password('password', ['class' => 'input']) }}
+    </div>
 
-
-
-
+    <div class="form-item">
+        {{ Form::submit('LOGIN', ['class' => 'submit-button']) }}
+    </div>
+    
+    <p><a href="/register">新規ユーザーの方はこちら</a></p>
+    {!! Form::close() !!}
+</div>
 @endsection
